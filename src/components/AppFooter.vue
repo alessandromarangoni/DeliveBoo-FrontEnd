71dc9243ddce130c78c32d5843eb2adb3617ec0a
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-08-28 13:23:02 +0200

    scaffolding generico

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: "AppFooter",
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: "AppMain",
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
