1157342e2d496b77f26e63d6f8e957fad686df7a
Edy Galdiolo <edygaldy302@gmail.com>
2023-08-28 18:23:36 +0200

    Fix: adaptive logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,7 +13,7 @@
     <div class="container">
         <!-- Logo -->
         <div class="row d-flex justify-content-center">
-            <div class="col-6 pt-3">
+            <div class="col-6 col-md-4 col-lg-3 p-3">
                 <img src="../assets/logo_deliveboo.png" class="img-fluid mx-auto d-block" alt="Logo">
             </div>
         </div>
@@ -21,5 +21,5 @@
 </template>
 
 <style scoped lang="scss">
-
+    
 </style>
