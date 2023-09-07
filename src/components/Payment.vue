6d13423c3bcc140b467858b5713627158fc4ef3a
alessandromarangoni <alessandromarangoni980@gmail.com>
2023-09-07 19:50:10 +0200

    style(credit card input)

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -40,14 +40,15 @@ export default {
                         client: clientInstance,
                         styles: {
                             input: {
-                                'font-size': '15px',
-                                'font-family': 'Open Sans'
-                            }
+                                'font-size': '30px',
+                                'font-family': 'Arial, Helvetica, sans-serif',
+                                'color': 'rgb(255, 255, 255)!important',
+                            },
                         },
                         fields: {
                             number: {
                                 selector: '#creditCardNumber',
-                                placeholder: '0000-0000-0000-0000'
+                                placeholder: '0000 0000 0000 0000'
                             },
                             cvv: {
                                 selector: '#cvv',
@@ -97,7 +98,6 @@ export default {
 
                 }).then(resp => {
                     // this.store.method.delete();
-                    // this.$router.push({ path: '/restaurants', query: { success: true } });
                     console.log(resp);
                     this.success = resp.data.success;
                     if(this.success == true ){
@@ -148,22 +148,62 @@ export default {
             <h1>Il tuo ordine è avvenuto con successo</h1>
         </div>
 
-        <form id="payment-form" @submit.prevent="sendPayment()" method="post" class="d-flex flex-column justify-content-center align-items-center mb-5">
+        <form id="payment-form" @submit.prevent="sendPayment()" method="post" class="d-flex flex-column justify-content-center text-white align-items-center mb-5">
 
-            <div class="w-75 m-auto pt-5 mt-5" id="dropin-container">
+            <div class="w-75 m-auto pt-5 mt-5 border p-4 position-relative card-background" id="dropin-container">
 
-                <label for="creditCardNumber" class="px-1 py-2">Numero Carta</label>
-                <div id="creditCardNumber" class="form-control mb-2 custom-imput "></div>
+                <svg version="1.1" class="chip" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 50 50" xml:space="preserve">  <image id="image0" width="50" height="50" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+              AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAB6VBMVEUAAACNcTiVeUKVeUOY
+              fEaafEeUeUSYfEWZfEaykleyklaXe0SWekSZZjOYfEWYe0WXfUWXe0WcgEicfkiXe0SVekSXekSW
+              ekKYe0a9nF67m12ZfUWUeEaXfESVekOdgEmVeUWWekSniU+VeUKVeUOrjFKYfEWliE6WeESZe0GS
+              e0WYfES7ml2Xe0WXeESUeEOWfEWcf0eWfESXe0SXfEWYekSVeUKXfEWxklawkVaZfEWWekOUekOW
+              ekSYfESZe0eXekWYfEWZe0WZe0eVeUSWeETAnmDCoWLJpmbxy4P1zoXwyoLIpWbjvXjivnjgu3bf
+              u3beunWvkFWxkle/nmDivXiWekTnwXvkwHrCoWOuj1SXe0TEo2TDo2PlwHratnKZfEbQrWvPrWua
+              fUfbt3PJp2agg0v0zYX0zYSfgkvKp2frxX7mwHrlv3rsxn/yzIPgvHfduXWXe0XuyIDzzISsjVO1
+              lVm0lFitjVPzzIPqxX7duna0lVncuHTLqGjvyIHeuXXxyYGZfUayk1iyk1e2lln1zYTEomO2llrb
+              tnOafkjFpGSbfkfZtXLhvHfkv3nqxH3mwXujhU3KqWizlFilh06khk2fgkqsjlPHpWXJp2erjVOh
+              g0yWe0SliE+XekShhEvAn2D///+gx8TWAAAARnRSTlMACVCTtsRl7Pv7+vxkBab7pZv5+ZlL/UnU
+              /f3SJCVe+Fx39naA9/75XSMh0/3SSkia+pil/KRj7Pr662JPkrbP7OLQ0JFOijI1MwAAAAFiS0dE
+              orDd34wAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfnAg0IDx2lsiuJAAACLElEQVRIx2Ng
+              GAXkAUYmZhZWPICFmYkRVQcbOwenmzse4MbFzc6DpIGXj8PD04sA8PbhF+CFaxEU8iWkAQT8hEVg
+              OkTF/InR4eUVICYO1SIhCRMLDAoKDvFDVhUaEhwUFAjjSUlDdMiEhcOEItzdI6OiYxA6YqODIt3d
+              I2DcuDBZsBY5eVTr4xMSYcyk5BRUOXkFsBZFJTQnp6alQxgZmVloUkrKYC0qqmji2WE5EEZuWB6a
+              lKoKdi35YQUQRkFYPpFaCouKIYzi6EDitJSUlsGY5RWVRGjJLyxNy4ZxqtIqqvOxaVELQwZFZdkI
+              JVU1RSiSalAt6rUwUBdWG1CP6pT6gNqwOrgCdQyHNYR5YQFhDXj8MiK1IAeyN6aORiyBjByVTc0F
+              qBoKWpqwRCVSgilOaY2OaUPw29qjOzqLvTAchpos47u6EZyYnngUSRwpuTe6D+6qaFQdOPNLRzOM
+              1dzhRZyW+CZouHk3dWLXglFcFIflQhj9YWjJGlZcaKAVSvjyPrRQ0oQVKDAQHlYFYUwIm4gqExGm
+              BSkutaVQJeomwViTJqPK6OhCy2Q9sQBk8cY0DxjTJw0lAQWK6cOKfgNhpKK7ZMpUeF3jPa28BCET
+              amiEqJKM+X1gxvWXpoUjVIVPnwErw71nmpgiqiQGBjNzbgs3j1nus+fMndc+Cwm0T52/oNR9lsdC
+              S24ra7Tq1cbWjpXV3sHRCb1idXZ0sGdltXNxRateRwHRAACYHutzk/2I5QAAACV0RVh0ZGF0ZTpj
+              cmVhdGUAMjAyMy0wMi0xM1QwODoxNToyOSswMDowMEUnN7UAAAAldEVYdGRhdGU6bW9kaWZ5ADIw
+              MjMtMDItMTNUMDg6MTU6MjkrMDA6MDA0eo8JAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDIzLTAy
+              LTEzVDA4OjE1OjI5KzAwOjAwY2+u1gAAAABJRU5ErkJggg=="></image>
+            </svg>
 
-                <label for="expireDate" class="px-1 py-2">Data di Scadenza</label>
-                <div id="expireDate" class="form-control mb-2 custom-imput "></div>
+                <div class="mx-5 text-white">
+                    <label for="creditCardNumber" class="px-1 pt-5">Numero Carta</label>
+                    <div id="creditCardNumber" class="form-control mb-2 custom-imput imput-carta border-0"></div>
+                </div>
 
-                <div @mouseenter="hover = true" @mouseleave="hover = false" class="cvv-input">
-                    <label for="cvv" class="px-1 py-2">CVV</label>
-                    <div id="cvv" class="form-control mb-2 custom-imput "></div>
+                <svg class="logo" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 48 48">
+                    <path fill="#ff9800" d="M32 10A14 14 0 1 0 32 38A14 14 0 1 0 32 10Z"></path><path fill="#d50000" d="M16 10A14 14 0 1 0 16 38A14 14 0 1 0 16 10Z"></path><path fill="#ff3d00" d="M18,24c0,4.755,2.376,8.95,6,11.48c3.624-2.53,6-6.725,6-11.48s-2.376-8.95-6-11.48 C20.376,15.05,18,19.245,18,24z"></path>
+                </svg>
+
+                <div class="d-flex mx-5 my-4 custom-margin-mobile">
+                    
+                    <div class="d-flex flex-column justify-content-center w-50">
+                        <label for="expireDate" class="text-white">Data di Scadenza</label>
+                        <div id="expireDate" class="form-control mb-2 custom-imput border-0 text-white"></div>
+                    </div>
+
+                    <div @mouseenter="hover = true" @mouseleave="hover = false" class="text-white w-50">
+                        <label for="cvv" class="">CVV</label>
+                        <div id="cvv" class="form-control mb-2 custom-imput border-0"></div>
+                    </div>
+
                 </div>
             </div>
-            <input class="custom-button btn bg-red text-white fw-bolder" type="submit" />
+            <input class="custom-button btn bg-red text-white fw-bolder mt-5" type="submit" />
 
         </form>
     </div>
@@ -182,7 +222,27 @@ export default {
 
 </template>
 
+
 <style scoped lang="scss">
+
+
+.card-background{
+    background: #3441ff;
+    border-radius:20px ;
+    aspect-ratio: 16 / 9 !important;
+}
+.logo{
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100px;
+    height: 80px;
+}
+
+.chip{
+    height: 50px!important;
+    width: 80px;
+}
 
     .bg-red{
         background-color: #f96a5f;
@@ -212,17 +272,19 @@ export default {
     }
     .custom-imput{
         height:40px;
-        border-radius:30px
+        border-radius:30px;
+        color: white!important;
+        background: #90d4ce00;
     }
     .custom-button{
         width: 20%;
     }
 
     .bg-custom {
-    background-color: #F96A5F;
-    margin-left: 100px;
-    width: calc(100% - 100px);
-    bottom: 0
+        background-color: #F96A5F;
+        margin-left: 100px;
+        width: calc(100% - 100px);
+        bottom: 0
     }
 
     .content {
@@ -236,14 +298,21 @@ export default {
 
     @media screen and (max-width:577px) {
 
+    .card-background{
+        padding: 0!important;
+
+        .chip{
+            margin-top: 10px;
+        }
+    }    
     .content {
         transform: translateY(-20%);
+        padding: 0;
     }
 
-    .content {
-        width: 70% !important;
+    .custom-margin-mobile{
+        margin-top: 0!important;
     }
-
     .footerWave {
         bottom: 80px;
     }
@@ -256,7 +325,7 @@ export default {
     }
 
     .content {
-        width: 90% !important;
+        width: 100% !important;
     }
 
 }
@@ -270,9 +339,11 @@ export default {
 
 @media screen and (min-width:1400px) {
     .content {
-        width: 30% !important;
+        width: 40% !important;
     }
-
+    .custom-margin-mobile{
+        margin-top: 10%!important;
+    }
     .transform {
         transform: translateY(-50%);
     }
